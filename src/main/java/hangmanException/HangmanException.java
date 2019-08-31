80788a47f5a2868861a02d9ce36bd88879c10fe2
unknown <2153043@labinfo.is.escuelaing.edu.co>
2019-08-31 10:13:49 -0500

    Clases de equivalencia

---
+++ src/main/java/hangmanException/HangmanException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangmanException;
+
+/**
+ *
+ * @author 2153043
+ */
+public class HangmanException extends Exception{
+    public static final String NUMERO_INVALIDO = "Un numero o mas en los parametros es incorrecto";
+    public HangmanException (String msg){
+        super(msg);
+    }
+    
+}
---
+++ src/test/java/hangman/GameScoreTest.java
@@ -0,0 +1,38 @@
+/** 
+ *OriginalScore:
+ * 
+Caso 1. Limite inferior: Cantidad Incorrectas = 11
+Caso 2. Limite medio: Cantidad Incorrectas = 5
+Caso 3. Limite superior:  cantidad Incorrectas = 0
+Caso 4. Cantidad incorrectas = -4
+
+BonusScore: 
+
+Caso 1. Limite inferior: Cantidad letras  correctas = 2 | cantidad Incorrectas = 5
+Caso 2. Limite medio: Cantidad letras  correctas = 5 | cantidad Incorrectas = 3
+Caso 3. Limite superior:  Cantidad letras  correctas = 6 |  cantidad Incorrectas = 0
+Caso 4. Cantidad correctas = -6 | Cantidad incorrectas 6
+Caso 5. Cantidad correctas = 6 | Cantidad incorrectas -6
+Caso 6. Cantidad correctas 5 | Cantidad incorrectas 15
+
+PowerBonusScore:
+
+Caso 1. Limite inferior: Cantidad correctas = 1 | Cantidad incorrectas = 2
+Caso 2. Limite medio: Cantidad correctas = 2 | Cantidad incorrectas = 1
+Caso 3. Limite superior: Cantidad correctas = 4 | Cantidad incorrectas 1
+Caso 4. Cantidad correctas = -6 | Cantidad incorrectas 6
+Caso 5. Cantidad correctas = 6 | Cantidad incorrectas -6
+ */
+package hangman;
+import org.junit.Assert;
+import hangmanException.*;
+import org.junit.Test;
+
+public class GameScoreTest {
+   @Test 
+    public void pruebaBonusScore(){
+        
+        
+    }
+
+}
