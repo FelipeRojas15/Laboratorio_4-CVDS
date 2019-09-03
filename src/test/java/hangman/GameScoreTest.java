318b112abf3b49edb93594c1b9b67b755988d61e
Kyrie <james.weisnner@mail.escuelaing.edu.co>
2019-09-03 13:33:03 -0500

    update especificacion pruebas

--- src/test/java/hangman/GameScoreTest.java
+++ src/test/java/hangman/GameScoreTest.java
@@ -1,27 +1,69 @@
 /** 
- *OriginalScore:
- * 
-Caso 1. Limite inferior: Cantidad Incorrectas = 11
-Caso 2. Limite medio: Cantidad Incorrectas = 5
-Caso 3. Limite superior:  cantidad Incorrectas = 0
-Caso 4. Cantidad incorrectas = -4
+  
+OriginalScore:
+* El puntaje no deberia ser menor a 0 asi hayan mas de 10 letras incorrectas
+   Caso 1. Limite inferior: Cantidad Incorrectas = 100 | puntaje = 0
+    Caso 2. Limite medio: Cantidad Incorrectas = 10    | puntaje = 0
+    Caso 3. Limite superior:  Cantidad Incorrectas = 0  | puntaje = 0
+ 
+* Deberia generarse un error al ingresar un numero de respuestas incorrectas negativo
+    * Caso 1. Limite inferior: Cantidad Incorrectas = -100  | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 2. Limite medio: Cantidad Incorrectas =-10       | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 3. Limite superior: Cantidad Incorrectas = -1    | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+ 
+* Deberia generarse un error al ingresar un numero de respuestas correctas negativo
+    * Caso 1. Limite inferior: Cantidad Correctas = -100  | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 2. Limite medio: Cantidad Correctas =-10       | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 3. Limite superior: Cantidad Correctas = -1    | deberia dar excepcion HangmanException.NUMERO_INVALIDO
 
 BonusScore: 
+* El puntaje no deberia ser menor a 0 asi hayan mas letras incorrectas que correctas
+    Caso 1. Limite inferior: Cantidad Correctas = 1 | Cantidad Incorrectas = 6  | puntaje = 0
+    Caso 2. Limite medio: Cantidad Correctas = 1    | Cantidad Incorrectas = 3  | puntaje = 0
+    Caso 3. Limite superior: Cantidad Correctas = 1 | Cantidad Incorrectas 4    | puntaje = 0
+    
+* No deberia haber limite de puntaje positivo.
+    * Caso 1. Limite inferior: Cantidad Correctas = 11 | Cantidad Incorrectas = 0  | puntaje = 110
+    * Caso 2. Limite medio: Cantidad Correctas = 15    | Cantidad Incorrectas = 0  | puntaje = 150
+    * Caso 3. Limite superior: Cantidad Correctas = 20 | Cantidad Incorrectas 0    | puntaje = 200
+    
+* Deberia generarse un error al ingresar un numero de respuestas incorrectas negativo
+    * Caso 1. Limite inferior: Cantidad Incorrectas = -100  | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 2. Limite medio: Cantidad Incorrectas =-10       | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 3. Limite superior: Cantidad Incorrectas = -1    | deberia dar excepcion HangmanException.NUMERO_INVALIDO 
 
-Caso 1. Limite inferior: Cantidad letras  correctas = 2 | cantidad Incorrectas = 5
-Caso 2. Limite medio: Cantidad letras  correctas = 5 | cantidad Incorrectas = 3
-Caso 3. Limite superior:  Cantidad letras  correctas = 6 |  cantidad Incorrectas = 0
-Caso 4. Cantidad correctas = -6 | Cantidad incorrectas 6
-Caso 5. Cantidad correctas = 6 | Cantidad incorrectas -6
-Caso 6. Cantidad correctas 5 | Cantidad incorrectas 15
+* Deberia generarse un error al ingresar un numero de respuestas correctas negativo
+    * Caso 1. Limite inferior: Cantidad Correctas = -100  | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 2. Limite medio: Cantidad Correctas =-10       | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 3. Limite superior: Cantidad Correctas = -1    | deberia dar excepcion HangmanException.NUMERO_INVALIDO     
+
 
 PowerBonusScore:
 
-Caso 1. Limite inferior: Cantidad correctas = 1 | Cantidad incorrectas = 2
-Caso 2. Limite medio: Cantidad correctas = 2 | Cantidad incorrectas = 1
-Caso 3. Limite superior: Cantidad correctas = 4 | Cantidad incorrectas 1
-Caso 4. Cantidad correctas = -6 | Cantidad incorrectas 6
-Caso 5. Cantidad correctas = 6 | Cantidad incorrectas -6
+* No deberia permitir tener mas de 500 puntos con ningun numero de respuestas correctas
+    * Caso 1. Limite inferior: Cantidad Correctas = 4 | Cantidad Incorrectas = 0 | puntaje = 500
+    * Caso 2. Limite medio: Cantidad Correctas = 5 | Cantidad Incorrectas = 0 | puntaje = 500
+    * Caso 3. Limite superior: Cantidad Correctas = 6 | Cantidad Incorrectas = 0| puntaje = 500
+
+* Deberia dar un puntaje mayor a 0
+    * Caso 1. Limite inferior: Cantidad Correctas = 2    | Cantidad Incorrectas 1 | puntaje = 17
+    * Caso 2. Limite medio: Cantidad Correctas = 3       | Cantidad Incorrectas 2 | puntaje = 500
+    * Caso 3. Limite superior: Cantidad Correctas = 4    | Cantidad Incorrectas 20 | puntaje = 500
+     
+* El puntaje no deberia ser menor a 0 asi hayan mas letras incorrectas que correctas
+    Caso 1. Limite inferior: Cantidad Correctas = 1 | Cantidad Incorrectas = 6  | puntaje = 0
+    Caso 2. Limite medio: Cantidad Correctas = 1    | Cantidad Incorrectas = 3  | puntaje = 0
+    Caso 3. Limite superior: Cantidad Correctas = 1 | Cantidad Incorrectas 4    | puntaje = 0    * 
+  
+* Deberia generarse un error al ingresar un numero de respuestas incorrectas negativo
+    * Caso 1. Limite inferior: Cantidad Incorrectas = -100  | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 2. Limite medio: Cantidad Incorrectas =-10       | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 3. Limite superior: Cantidad Incorrectas = -1    | deberia dar excepcion HangmanException.NUMERO_INVALIDO 
+
+* Deberia generarse un error al ingresar un numero de respuestas correctas negativo
+    * Caso 1. Limite inferior: Cantidad Correctas = -100  | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 2. Limite medio: Cantidad Correctas =-10       | deberia dar excepcion HangmanException.NUMERO_INVALIDO
+    * Caso 3. Limite superior: Cantidad Correctas = -1    | deberia dar excepcion HangmanException.NUMERO_INVALIDO      
  */
 package hangman;
 import org.junit.Assert;
